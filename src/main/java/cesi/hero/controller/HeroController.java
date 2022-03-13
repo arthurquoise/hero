20237edea6085be79311db8bf1dcb893a3016703
Arthur <arthur.dennetiere@gmail.com>
2022-03-13 16:15:08 +0100

    feat: registration form

--- src/main/java/cesi/hero/controller/HeroController.java
+++ src/main/java/cesi/hero/controller/HeroController.java
@@ -1,10 +1,14 @@
 package cesi.hero.controller;
 
 import cesi.hero.model.Hero;
+import cesi.hero.model.Incident;
 import cesi.hero.repositories.HeroRepository;
+import cesi.hero.repositories.IncidentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -13,9 +17,11 @@ public class HeroController {
 
     final
     HeroRepository _heroRepository;
+    IncidentRepository _incidentRepository;
 
-    public HeroController(HeroRepository _heroRepository) {
+    public HeroController(HeroRepository _heroRepository, IncidentRepository _incidentRepository) {
         this._heroRepository = _heroRepository;
+        this._incidentRepository = _incidentRepository;
     }
 
     @RequestMapping("/")
@@ -23,5 +29,19 @@ public class HeroController {
         List<Hero> heroes = (List<Hero>) _heroRepository.findAll();
         model.addAttribute("heroes", heroes);
         return "heroes";
+    }
+
+    @RequestMapping("/register")
+    public String RegisterForm(Model model){
+        List<Incident> incidents = (List<Incident>) this._incidentRepository.findAll();
+        model.addAttribute("hero", new Hero());
+        model.addAttribute("incidents", incidents);
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String RegisterSubmit(@ModelAttribute Hero hero, Model model){
+
+    return "";
     }
 }
